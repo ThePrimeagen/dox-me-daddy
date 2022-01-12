00cc1987a10590b6afff2cdeebe459c54cdc6e70
mpaulson <mpaulson@netflix.com>
2022-01-12 14:14:39 -0700

    feat: test client simplified

--- src/bin/test_client.rs
+++ src/bin/test_client.rs
@@ -19,31 +19,11 @@ async fn main() -> Result<(), DoxMeDaddyError> {
 
     let (socket, _) = connect_async(Url::parse(url.as_str()).unwrap()).await?;
 
-    let (outgoing, mut incoming) = socket.split();
-    let (tx, rx) = futures_channel::mpsc::unbounded();
+    let (_, mut incoming) = socket.split();
 
-    let write_outgoing = rx.map(Ok).forward(outgoing);
-    let handle = tokio::spawn(async move {
-        while let Some(msg) = incoming.next().await {
-            info!("Message Received {:?}", msg);
-            /*
-             * NOTE: uncomment to just send back the messages I got
-             inner_tx
-             .unbounded_send(msg.unwrap().clone())
-             .expect("transactions to be successful");
-            */
-        };
-    });
-
-    pin_mut!(write_outgoing, handle);
-    tokio::spawn(async move {
-        loop {
-            std::thread::sleep(Duration::from_secs(5));
-            println!("sending message up");
-            tx.unbounded_send(Message::Text("hello".to_string())).expect("test client send failed");
-        }
-    });
-    future::select(write_outgoing, handle).await;
+    while let Some(msg) = incoming.next().await {
+        println!("Message Received {:?}", msg);
+    };
 
     return Ok(());
 }
